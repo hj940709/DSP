08a467c3de78376a389e854ce56f8e46654afd07
hj940079 <hj940709@gmail.com>
2017-02-25 14:46:54 +0200

    Feb-25

--- src/com/hj/PlotServlet.java
+++ src/com/hj/PlotServlet.java
@@ -23,16 +23,25 @@ public class PlotServlet extends HttpServlet {
 		String equation = request.getParameter("equation");
 		String coefficient = new SinParser(equation).getCoefficient();
 		String path = getServletContext().getRealPath("/");
-//		path = path.replace("\\", "/");
 		try {
 			// from -pi to pi with step size pi/40 (0.0785398163397)
-			//Execute r.sh
+			//Execute r.sh or command line
 			//r.sh :Rcript -e "jpeg('$filepath');plot(seq(-pi,pi,pi/40),$a*seq(-pi,pi,pi/40),main='y=$equation',type='l');dev.off()";
 			//Plotting to a jpeg figure
-			new ProcessBuilder(path+"r.sh",	path+"Rplot.jpeg",coefficient,equation).start().waitFor();
-//			new ProcessBuilder("Rscript","-e","\"jpeg('"+path+"Rplot.jpeg"+"');"
-//					+ "plot(seq(-pi,pi,pi/40),"+coefficient+"*sin(seq(-pi,pi,pi/40))"+
-//					",main='y="+equation+"',xlab='X',ylab='Y',type='l');\"").start().waitFor();
+			String os = System.getProperties().getProperty("os.name");
+			os = os.split(" ")[0];
+			switch(os){
+			case "Windows":
+				path = path.replace("\\", "/");
+				new ProcessBuilder("Rscript","-e","\"jpeg('"+path+"Rplot.jpeg"+"');"
+						+ "plot(seq(-pi,pi,pi/40),"+coefficient+"*sin(seq(-pi,pi,pi/40))"+
+						",main='y="+equation+"',xlab='X',ylab='Y',type='l');\"").start().waitFor();
+				break;
+				
+			case "Linux":
+				new ProcessBuilder(path+"r.sh",	path+"Rplot.jpeg",coefficient,equation).start().waitFor();
+				break;
+			}
 			//Output and send the figure to client
 			ImageIO.write(ImageIO.read(new File(path+"Rplot.jpeg")),
 					"jpeg", response.getOutputStream());
